85f800d5d8699abe8f3448e7d3bb5d205cb48cc7
sfy <1923554655@qq.com>
2023-11-14 01:47:03 +0800

    了解汇编程序——机器码

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// 3*2 换算成 2个3相加
+    // 初始化
+    @2
+    M = 0
+
+    // 判断是否小于0
+    @0
+    D = M
+    @END
+    D;JLT
+    //declaring a label
+(LOOP)
+    @1
+    M = M - 1
+    D = M
+    @END
+    D;JLT
+
+    @0
+    D = M
+    @2
+    M = M + D
+    //use a label
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
